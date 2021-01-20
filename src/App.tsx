d6fd4f0ff61a5402a860cfbdd79bf925d5d4a823
michel ribeiro <mlribeiro@icatuseguros.com.br>
2021-01-20 16:56:43 -0300

    base react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, Fragment} from 'react';
+import axios from 'axios';
+import './App.scss';
 
-function App() {
+interface LocationAllow {
+  answer: boolean;
+}
+
+const App: React.FC = () => {
+  const [location, setLocation] = useState<LocationAllow>({answer: false});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setLocation({answer: true})
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h3>O clima em </h3>
+    </Fragment>
   );
 }
 
